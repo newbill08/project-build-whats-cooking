0fecfa4967cee05274473ad54c39589f085e7018
joshua <josh.newbill08@hotmail.com>
2023-07-02 01:07:24 -0500

    get render RecipeCard and all styles complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 //  Elements
 const cuisineSelect = document.querySelector("#cuisines")
 const categorySelect = document.querySelector("#categories")
+const recipeContainer = document.querySelector(".recipe-container")
 
 //Function Calls
 getCuisines()
@@ -40,7 +41,7 @@ function renderCuisineOptions(cuisines) {
 }
 
 // Category
-function getCategories(){
+function getCategories() {
     fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
         .then(r => r.json())
         .then(categories => renderCategoryOptions(categories.meals))
@@ -63,27 +64,57 @@ function renderCategoryOptions(categories) {
 
 // Recipe Collection Functions
 
-function getRecipesByCuisine (e){
-const cusine = e.target.value
- //   debugger
+function getRecipesByCuisine(e) {
+    const cusine = e.target.value
+    //   debugger
 
-  fetch  (`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cusine}`)
-  .then(r => r.json())
-  .then(recipes => renderAllRecipes(recipes.meals))
-    // .then(recipes => console.log(recipes.meals))
-    .catch(error => alert(error))
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cusine}`)
+        .then(r => r.json())
+        .then(recipes => renderAllRecipes(recipes.meals))
+        // .then(recipes => console.log(recipes.meals))
+        .catch(error => alert(error))
 }
 
-function renderAllRecipes(recipes){
-  //  console.log(recipes)
-  recipes.forEach(recipe => {
-   console.log(recipe)
-    // renderRecipeCard(recipe)
-  })
+function renderAllRecipes(recipes) {
+    recipeContainer.replaceChildren()
+    console.log(recipes)
+    recipes.forEach(recipe => {
+        renderRecipeCard(recipe)
+        // console.log(recipe)
+
+    })
+    cuisineSelect.value = ""
+    categorySelect.value = ""
+}
+
+function renderRecipeCard(recipe) {
+    const { 
+        idMeal: recipeId,
+        strMeal: recipeName,
+        strMealThumb: recipeImage, 
+    } = recipe
+    console.log(recipe)
+    //console.log(idMeal)
+    // console.log(strMeal)
+    // console.log(strMealThumb)
+
+    const cardDiv = document.createElement("div")
+    cardDiv.classList.add("card")
+    // add event listener to card
+   
+    const image = document.createElement("img")
+    image.src = recipeImage
+
+    const title = document.createElement("h3")
+    title.textContent = recipeName
+    
+    
+    cardDiv.append(image, title)
+    recipeContainer.append(cardDiv)
 
 }
 
-function getRecipesByCategories (e){
+function getRecipesByCategories(e) {
 
 
 }
