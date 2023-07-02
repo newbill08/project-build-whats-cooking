e5536a3c3e5fa611e02d4f33ed4bbfc7ad23d15e
joshua <josh.newbill08@hotmail.com>
2023-07-02 00:19:50 -0500

    get recipes by cuisines

--- index.js
+++ index.js
@@ -2,52 +2,89 @@
 
 //  Elements
 const cuisineSelect = document.querySelector("#cuisines")
-const CategorySelect = document.querySelector("#categories")
+const categorySelect = document.querySelector("#categories")
 
 //Function Calls
 getCuisines()
 getCategories()
 
+//Add Eventlistioner---------------------------
 
-function getCuisines(){
-fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
-.then(r => r.json())
-//.then(cuisines => console.log(cuisines))
-.then(cuisines => renderCuisineOptions(cuisines.meals))
-.catch(error => alert(error))
+cuisineSelect.addEventListener("change", getRecipesByCuisine)
+//categorySelect.addEventListener("change", getRecipesByCategories)
+
+//EventL End---------------------------------
+
+
+function getCuisines() {
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list")
+        .then(r => r.json())
+        //.then(cuisines => console.log(cuisines))
+        .then(cuisines => renderCuisineOptions(cuisines.meals))
+        .catch(error => alert(error))
 
 }
 
-function renderCuisineOptions(cuisines){
-    console.log(cuisines)
+function renderCuisineOptions(cuisines) {
+    /*console.log(cuisines)
     console.log("In render")
+    */
 
     // Go through array and make option for it
     cuisines.forEach(cuisine => {
         const option = document.createElement("option")
         option.value = cuisine.strArea
         option.textContent = cuisine.strArea
-        cuisineSelect.append(option)        
+        cuisineSelect.append(option)
     });
 }
 
 // Category
 function getCategories(){
     fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
-    .then(r => r.json())
-    .then(categories => renderCategoryOptions(categories.meals))
+        .then(r => r.json())
+        .then(categories => renderCategoryOptions(categories.meals))
+        .catch(error => alert(error))
+
+}
+
+function renderCategoryOptions(categories) {
+    /*console.log("categories: ")
+    console.log(categories)
+    */
+
+    categories.forEach(category => {
+        const option = document.createElement("option")
+        option.value = category.strCategory
+        option.textContent = category.strCategory
+        categorySelect.append(option)
+    });
+}
+
+// Recipe Collection Functions
+
+function getRecipesByCuisine (e){
+const cusine = e.target.value
+ //   debugger
+
+  fetch  (`https://www.themealdb.com/api/json/v1/1/filter.php?a=${cusine}`)
+  .then(r => r.json())
+  .then(recipes => renderAllRecipes(recipes.meals))
+    // .then(recipes => console.log(recipes.meals))
     .catch(error => alert(error))
-    
-    }
+}
 
-    function renderCategoryOptions(categories){
-        console.log("categories: ")
-        console.log(categories)
+function renderAllRecipes(recipes){
+  //  console.log(recipes)
+  recipes.forEach(recipe => {
+   console.log(recipe)
+    // renderRecipeCard(recipe)
+  })
 
-        categories.forEach(category => {
-            const option = document.createElement("option")
-            option.value = category.strCategory
-            option.textContent = category.strCategory
-            CategorySelect.append(option)        
-        });
-    }
+}
+
+function getRecipesByCategories (e){
+
+
+}
+
